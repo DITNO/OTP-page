a6d8dabb9546b0cdd02de275273c576056651718
DITNO <92884446+DITNO@users.noreply.github.com>
2023-05-05 23:19:01 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import OtpInput from './component/OtpInput';
+import './App.css';
+
+export default function App() {
+  const [otp, setOtp] = useState('');
+  const onChange = (value: string) => setOtp(value);
+
+  return (
+    <div className="container">
+      <h1>Please enter OTP here</h1>
+      <OtpInput value={otp} valueLength={6} onChange={onChange} />
+      <button>Verify</button>
+    </div>
+  );
+}
